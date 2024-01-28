b2fc8725ff7d1be247f0299c8841c6566cb97235
shavear <109918581+shavear@users.noreply.github.com>
2024-01-28 10:51:18 +0000

    feat: Fix import order in email template, add preview conditional in email template, style changes in email template, fix button href url in email template, add devcontainer for Node.js, fix baseUrl value in email template and add Dependabot configuration file

--- emails/email-template.tsx
+++ emails/email-template.tsx
@@ -1,17 +1,17 @@
 import {
-  Html,
-  Head,
   Body,
-  Container,
   Button,
-  Preview,
+  Container,
+  Head,
   Heading,
-  Text,
+  Html,
+  Preview,
+  Row,
   Section,
-  Row
+  Text,
 } from "@react-email/components";
 import { Tailwind } from "@react-email/tailwind";
-import * as React from 'react';
+import * as React from "react";
 
 interface EmailProps {
   name: string;
@@ -19,37 +19,57 @@ interface EmailProps {
   message: string;
 }
 
-export default function EmailTemplate({ name, email, message }: EmailProps) {
+const baseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "";
+
+export const EmailTemplate = ({ name, email, message }: EmailProps) => {
   return (
     <Html>
       <Head />
-      <Preview>{email && email}</Preview>
-      <Tailwind>
-        <Body>
-          <Container className="flex items-center justify-center container mx-auto">
-            <Section className="flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900 rounded shadow-xl p-8 border border-gray-900 dark:border-gray-100 gap-y-6 dark:text-white max-w-min">
-              <Heading as="h1" className="text-6xl">Contact</Heading>
-              <Row className="flex flex-col items-start justify-center gap-y-6">
+      <Preview>{email ? email : "website"}</Preview>
+      <Tailwind
+        config={{
+          theme: {
+            extend: {
+              colors: {
+                brand: "#2250f4",
+                offwhite: "#fafbfb",
+              },
+            },
+          },
+        }}
+      >
+        <Body className="bg-offwhite text-base font-sans">
+          <Container className="flex items-center justify-center container mx-auto bg-gray-100 dark:bg-gray-900 rounded shadow-xl p-8 border border-gray-900 dark:border-gray-100 gap-y-6 dark:text-white max-w-min">
+            <Heading className="text-6xl text-center">Contact</Heading>
+            <Section className="flex flex-col items-start justify-center gap-y-6">
+              <Row>
                 <Text>
-                  <strong>Name:{" "}</strong>
+                  <strong>Name: </strong>
                   {name && name}
                 </Text>
                 <Text>
-                  <strong>
-                    Email:{" "}
-                  </strong>
+                  <strong>Email: </strong>
                   {email && email}
                 </Text>
                 <Text>
-                  <strong>Message:{" "}</strong>
+                  <strong>Message: </strong>
                   {message && message}
                 </Text>
               </Row>
-              <Button href="http://localhost:3000" className="max-w-min outline outline-2 outline-black dark:outline-white hover:outline-offset-4 bg-black dark:bg-white text-white dark:text-black transition-all text-xl rounded-full px-6 py-2">Back</Button>
+              <Button
+                href={baseUrl}
+                className="max-w-min outline outline-2 outline-black dark:outline-white hover:outline-offset-4 bg-black dark:bg-white text-white dark:text-black transition-all text-xl rounded-full px-6 py-2"
+              >
+                Back
+              </Button>
             </Section>
           </Container>
         </Body>
       </Tailwind>
     </Html>
-  )
-}
+  );
+};
+
+export default EmailTemplate;
