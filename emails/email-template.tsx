112888be322bcacbc36e6417308bf517b3ede9b1
shavear <wolfkurd66@gmail.com>
2024-01-28 13:02:35 +0300

    chore(.gitignore): remove unused entries and fix newline issue in .gitignore file
    style(email-template.tsx): organize imports and structure, use Section and Row components for better readability
    
    feat(package.json): add @react-email/tailwind as a dependency with version ^0.0.14

--- emails/email-template.tsx
+++ emails/email-template.tsx
@@ -1,5 +1,4 @@
 import {
-  Tailwind,
   Html,
   Head,
   Body,
@@ -8,7 +7,11 @@ import {
   Preview,
   Heading,
   Text,
+  Section,
+  Row
 } from "@react-email/components";
+import { Tailwind } from "@react-email/tailwind";
+import * as React from 'react';
 
 interface EmailProps {
   name: string;
@@ -19,16 +22,14 @@ interface EmailProps {
 export default function EmailTemplate({ name, email, message }: EmailProps) {
   return (
     <Html>
+      <Head />
+      <Preview>{email && email}</Preview>
       <Tailwind>
-        <Head>
-          <title>Contact Email</title>
-          <Preview>{email && email}</Preview>
-        </Head>
         <Body>
           <Container className="flex items-center justify-center container mx-auto">
-            <div className="flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900 rounded shadow-xl p-8 border border-gray-900 dark:border-gray-100 gap-y-6 dark:text-white max-w-min">
+            <Section className="flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900 rounded shadow-xl p-8 border border-gray-900 dark:border-gray-100 gap-y-6 dark:text-white max-w-min">
               <Heading as="h1" className="text-6xl">Contact</Heading>
-              <div className="flex flex-col items-start justify-center gap-y-6">
+              <Row className="flex flex-col items-start justify-center gap-y-6">
                 <Text>
                   <strong>Name:{" "}</strong>
                   {name && name}
@@ -43,9 +44,9 @@ export default function EmailTemplate({ name, email, message }: EmailProps) {
                   <strong>Message:{" "}</strong>
                   {message && message}
                 </Text>
-              </div>
-              <Button href="http://localhost:3000" className="max-w-min outline outline-2 outline-black dark:outline-white hover:outline-offset-4 bg-black dark:bg-white text-white dark:text-black transition-all text-xl rounded-full px-6 py-2 flex">Go back</Button>
-            </div>
+              </Row>
+              <Button href="http://localhost:3000" className="max-w-min outline outline-2 outline-black dark:outline-white hover:outline-offset-4 bg-black dark:bg-white text-white dark:text-black transition-all text-xl rounded-full px-6 py-2">Back</Button>
+            </Section>
           </Container>
         </Body>
       </Tailwind>
